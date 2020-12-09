f59426593db2fb51ec8207001635b027e20ae305
martindzida <45061806+martindzida@users.noreply.github.com>
2020-12-09 14:23:21 +0100

    Update getImage.js

--- src/redux/reducers/getImage.js
+++ src/redux/reducers/getImage.js
@@ -1,16 +1,20 @@
 import { GET_IMAGE } from "../actions/types";
 
+//Defaultní stav
 const initialState = {
   image: {},
 };
 
+//Reducer
 export default function (state = initialState, action) {
+  //Reducer vykoná příchozí akci
   switch (action.type) {
     case GET_IMAGE:
       return {
         ...state,
         image: action.payload,
       };
+    //Pokud se spustí jiná akce, reducer vrátí defaultní stav  
     default:
       return state;
   }

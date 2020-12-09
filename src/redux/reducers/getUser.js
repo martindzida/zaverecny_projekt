dda1c6c27b5ece725df2eff0c21b3dc0856b5d21
martindzida <45061806+martindzida@users.noreply.github.com>
2020-12-09 14:26:02 +0100

    Update getUser.js

--- src/redux/reducers/getUser.js
+++ src/redux/reducers/getUser.js
@@ -1,16 +1,20 @@
 import { GET_USER } from "../actions/types";
 
+//Defaultní stav
 const initialState = {
   users: {},
 };
 
+//Reducer
 export default function (state = initialState, action) {
+  //Reducer vykoná příslušnou akci
   switch (action.type) {
     case GET_USER:
       return {
         ...state,
         user: action.payload,
       };
+    //Pokud se spustí jiná akce, reducer vrátí defaultní stav  
     default:
       return state;
   }

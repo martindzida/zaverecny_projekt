8e5986afd7ca992a38d7e22809d04a2647fda478
martindzida <45061806+martindzida@users.noreply.github.com>
2020-12-09 14:11:17 +0100

    Update actions.js

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,15 +1,18 @@
 import { GET_TOKEN, GET_USER, GET_IMAGE } from "./types";
 
+//Akce, sloužící k nahrání přihlašovacího tokenu do storu
 export const getToken = (token) => ({
   type: GET_TOKEN,
   payload: token,
 });
 
+//Akce, sloužící k nahrání informací o uživateli do storu
 export const getUser = (user) => ({
   type: GET_USER,
   payload: user,
 });
 
+//Akce, sloužící k nahrání obrázku do storu (pouze zkušební, přemění se v nastavení profilu)
 export const getImage = (image) => ({
   type: GET_IMAGE,
   payload: image,
